5faa9b7ee8e0cf72a4dbe2ab26afcd54a42ff216
Stuart Butler <stuart.butler01@sap.com>
2023-12-01 16:50:04 +0000

    Add astrology questions to main quiz

--- questions.js
+++ questions.js
@@ -2,12 +2,14 @@ import { nextQuestion as nextGeneralQuestion } from "./general/questions.js";
 import { nextQuestion as nextPokemonQuestion } from "./poke/questions.js";
 import { nextQuestion as nextHarryPotterQuestion } from "./hp/questions.js";
 import { nextQuestion as nextFlagQuestion } from "./flags/questions.js";
+import { nextQuestion as nextAstrologyQuestion } from "./astrology/questions.js";
 
 const weights = [
     { weight: 20, fn: nextGeneralQuestion },
     { weight: 1, fn: nextPokemonQuestion },
     { weight: 1, fn: nextHarryPotterQuestion },
-    { weight: 2, fn: nextFlagQuestion }
+    { weight: 2, fn: nextFlagQuestion },
+    { weight: 1, fn: nextAstrologyQuestion }
 ];
 
 let currentWeightedPosition = 0;
